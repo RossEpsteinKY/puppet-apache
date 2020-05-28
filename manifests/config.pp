8664cf163bb1b8cb09dc5429fc60ad909f2011b5
root <root@rossepsteinforge1c.mylabserver.com>
2020-05-28 19:24:56 +0000

    added configs

---
+++ manifests/config.pp
@@ -0,0 +1,14 @@
+# @summary A short summary of the purpose of this class
+#    manages apache configs for puppet
+
+
+class apache::config {
+  file { 'apache_config':
+    ensure => $apache::config_ensure,
+    path   => $apache::config_path,
+    source => "puppet:///modules/apache/${osfamily}.conf",
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+}
